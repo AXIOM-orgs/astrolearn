8d2df526288d100897ca5d64f202ac852a6f7ee0
Fajarihsan <7afajarihsan@gmail.com>
2026-04-15 04:11:56 +0000

    Edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "Admin",
+            name: "Axiom",
             script: "node_modules/next/dist/bin/next",
             args: "start -p 3003",
             cwd: "./",
